9d85c5a39893ccec0170e7a3e3b3d3b5b1e53e91
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-03-06 18:52:47 +0700

    sale master fc

---
+++ src/controllers/saleMaster.js
@@ -0,0 +1,19 @@
+
+const conn = require("../config/dbconn");
+const sale=async(req,res)=>{
+    const body=req.body;
+    console.log("//::::::::::::::SALE MASTER::::::::::::::");
+    const txnList=body.txn;
+    const userId=userId;
+    console.log("Txn len: " +txnList.length);
+    console.log("User id: " +userId);
+    res.send("well recieved");
+
+
+
+
+}
+
+module.exports={
+    sale,
+}
