bf353df61781b6e87dd3775068c77f40ffbd051a
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-03-07 13:02:05 +0700

    sale master fc

--- src/controllers/saleMaster.js
+++ src/controllers/saleMaster.js
@@ -12,7 +12,7 @@ const sale = async (req, res) => {
     for (el of txnList) {
         const responseCode = await isOverLuckNum(el);
         console.log("RESPONSE FULL CHECK" + responseCode);
-        console.log("STATUS: "+responseCode.status);
+        console.log("STATUS: " + responseCode.status);
         if (responseCode.status == "00") {
             errorList.push(responseCode);
             console.log("ELEMENT LOOPING...");
@@ -21,7 +21,7 @@ const sale = async (req, res) => {
 
     }
     console.log("FINAL PROCESS NO ERROR ");
-    console.log("ERROR.LEN "+errorList.length);
+    console.log("ERROR.LEN " + errorList.length);
     if (errorList) return res.json({ status: "00", data: errorList });
     res.send("Transaction completed");
 
@@ -37,8 +37,8 @@ const isOverLuckNum = async (txn) => {
     const subcat = txn.subcategory;
     const ismId = txn.ismId;
     let maxType = '';
-    let response = {'status':"00","error":""};
-    console.log("ism: "+ismId+" subcat: "+subcat+" category: "+category+" userId: "+userId);
+    let response = { 'status': "00", "error": "" };
+    console.log("ism: " + ismId + " subcat: " + subcat + " category: " + category + " userId: " + userId);
     switch (luckNum.length) {
         case 1:
             maxType = subCatCheck(luckNum)
@@ -63,27 +63,31 @@ const isOverLuckNum = async (txn) => {
     }
     console.log("SWITCHING: " + maxType);
     let sqlCom = `SELECT SUM(sale_price) AS recent_sale FROM sale WHERE  ism_id =${ismId} AND sub_cat_id=${subcat}  AND sale_num = ${luckNum}`;
-    await Db.query(sqlCom, (er, re) => {
+    try {
 
-        if (er) {
-            console.log("error: SELECT RECECNT SALE" + er);
-            response = { "status": "05", "error": "server error" + er };
-        }
-        
-        const recentSale = re[0]["recent_sale"]
-        let maxSale = 0;
-        console.log("RECENT SALE: "+recentSale);
-        //FIND MAX SALE
-        sqlCom = `SELECT ${maxType} FROM salelimit;`;
-        Db.query(sqlCom, (er, re) => {
-            if (er) return { "status": "05", "error": "server error" + er };
-            maxSale = re[0][maxType];
-            console.log("MAX SALE: "+maxSale);
-            if (maxSale > recentSale + amount) response= { "status": "05", "error": luckNum + " is over maximum" }
-            response= { "status": "00","error":"" };
-        })
+        const q_res = await Db.query(sqlCom);
+     
 
-    })
+  
+            console.log("Q_RES"+q_res);
+            const recentSale = q_res[0]["recent_sale"]
+            let maxSale = 0;
+            console.log("RECENT SALE: "+recentSale);
+            //FIND MAX SALE
+            sqlCom = `SELECT ${maxType} FROM salelimit;`;
+            await Db.query(sqlCom, (er, re) => {
+                if (er) return { "status": "05", "error": "server error" + er };
+                maxSale = re[0][maxType];
+                console.log("MAX SALE: "+maxSale);
+                if (maxSale > recentSale + amount) response= { "status": "05", "error": luckNum + " is over maximum" }
+                response= { "status": "00","error":"" };
+            })
+    
+
+    } catch (error) {
+        console.log("Error: " + error);
+        response = { "status": "05", "error": "server error" + er };
+    }
     return response;
 
 
