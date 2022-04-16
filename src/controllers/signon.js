0f2d56707a27f9e1564213370acfe13138f72668
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-04-16 19:43:46 +0700

    login

--- src/controllers/signon.js
+++ src/controllers/signon.js
@@ -10,6 +10,7 @@ const login = async (req, res) => {
     const sqlCom = `SELECT mem_id,brc_code,group_code,mem_pass,mem_name,mem_lname,mem_tel FROM member WHERE mem_id ='${userId}'`;
     Db.query(sqlCom, (er, re) => {
         if (er) return res.json({ status: "05", desc: er })
+        if (re.length==0) return res.json("ໄອດີບໍ່ ຖືກຕ້ອງ")
         const realPassword = re[0]["mem_pass"];
         const credential = { userId: re[0]["mem_id"], brcCode: re[0]["brc_code"], groupCode: re[0]["group_code"], userName: re[0]["mem_name"], userLname: re[0]["mem_lname"], userTel: re[0]["mem_tel"], }
         try {
