0f7f683183a92dff878bc868d13f51dc1de2cebb
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-04-16 19:46:13 +0700

    login

--- src/controllers/signon.js
+++ src/controllers/signon.js
@@ -10,7 +10,7 @@ const login = async (req, res) => {
     const sqlCom = `SELECT mem_id,brc_code,group_code,mem_pass,mem_name,mem_lname,mem_tel FROM member WHERE mem_id ='${userId}'`;
     Db.query(sqlCom, (er, re) => {
         if (er) return res.json({ status: "05", desc: er })
-        if (re.length==0) return res.json("ໄອດີບໍ່ ຖືກຕ້ອງ")
+        if (re.length==0) return res.json("Error: ໄອດີບໍ່ ຖືກຕ້ອງ")
         const realPassword = re[0]["mem_pass"];
         const credential = { userId: re[0]["mem_id"], brcCode: re[0]["brc_code"], groupCode: re[0]["group_code"], userName: re[0]["mem_name"], userLname: re[0]["mem_lname"], userTel: re[0]["mem_tel"], }
         try {
@@ -18,8 +18,9 @@ const login = async (req, res) => {
             if (isAuth) {
                 return res.json(hook.authen.generateToken(credential));
             }
+            res.json("Error: ລະຫັດຜ່ານບໍ່ຖືກຕ້ອງ")
         } catch (error) {
-            res.json({ status: "01", desc: "invalid password" })
+            res.json("Error: ເກີດຂໍ້ຜິດພາດ ທາງເຊີເວີ (ບໍ່ສາມາດ ປຽບທຽບລະຫັດຜ່ານ)")
         }
     })
 }
