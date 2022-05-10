47abbcad312bcd1480662a0f076d790824ffabf1
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-05-10 19:32:04 +0700

    update new sale master

--- src/controllers/saleMaster.js
+++ src/controllers/saleMaster.js
@@ -40,6 +40,7 @@ const sale = async (req, res) => {
 const processTxn = async (txnList, barCode, res,catId,subCatId) => {
     sqlCom = 'INSERT INTO `sale`(`sale_bill_id`,`cat_id`,`sub_cat_id`, `ism_id`, `sale_num`, `sale_price`, `mem_id`, `client_date`,`qr_code`) VALUES ';
     const bill_num = await getBillnum();
+    if(!bill_num)return res.json({status:"04",desc:"Transaction fail: cannot get ticket number"});
     for (let i = 0; i < txnList.length; i++) {
         const colon = i < txnList.length - 1 ? "," : ";";
         let txn = txnList[i];
@@ -50,8 +51,7 @@ const processTxn = async (txnList, barCode, res,catId,subCatId) => {
             console.log("RESULT SQL: " + er);
             return res.json({ status: "05", data:[{error:er} ]  })
         }
-        console.log("Transaction inserted");
-        res.json([{ status: "00", desc: "Transaction completed" }]);
+        res.json([{ status: "00", desc: "Transaction completed",ticket_number: bill_num}]);
     })
     console.log("FINAL SQL COMMAND: " + sqlCom);
 
