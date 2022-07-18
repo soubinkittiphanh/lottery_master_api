ffa5d7d82c93876c7e34a1eadc143d38d40a67fb
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-07-18 14:05:06 +0700

    update sale master report api

--- src/controllers/report_master.js
+++ src/controllers/report_master.js
@@ -3,7 +3,10 @@ const db=require('../config/dbconn');
 const saleReport=async(req,res)=>{
     const {memId,fromIsmDate} = req.query;
 
-    const sqlCmd=`SELECT s.* FROM sale s WHERE s.ism_id IN (SELECT ism_id FROM installment WHERE ism_date >='${fromIsmDate} 00:00:00' ) AND s.mem_id='${memId}' `;
+    const sqlCmd=`SELECT s.*,sc.scat_name FROM sale s 
+    LEFT JOIN sub_category sc ON sc.cat_id=s.sub_cat_id
+    WHERE s.ism_id IN (SELECT ism_id FROM installment WHERE ism_date >='${fromIsmDate} 00:00:00' ) 
+    AND s.mem_id='${memId}' `;
     console.log(`Loading data ism: '${fromIsmDate}' mem: '${memId}'`);
     console.log("QUERY: ",sqlCmd);
     db.query(sqlCmd,(er,re)=>{
