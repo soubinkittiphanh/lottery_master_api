001d5222ae2f8631fdb8e79351baf13497018a9d
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-07-28 20:43:02 +0700

    master query wrong

--- src/controllers/report_master.js
+++ src/controllers/report_master.js
@@ -3,9 +3,10 @@ const db=require('../config/dbconn');
 const saleReport=async(req,res)=>{
     const {memId,fromIsmDate} = req.query;
 
-    const sqlCmd=`SELECT s.*,sc.scat_name,c.category_name FROM sale s
+    const sqlCmd=`SELECT s.*,sc.scat_name,c.category_name,i.ism_date FROM sale s
     LEFT JOIN t_category c ON s.cat_id=c.category_id 
     LEFT JOIN sub_category sc ON sc.scat_id=s.sub_cat_id
+    LEFT JOIN installment i ON i.ism_ref=s.ism_id
     WHERE s.ism_id IN (SELECT ism_id FROM installment WHERE ism_date >='${fromIsmDate} 00:00:00' ) 
     AND s.mem_id='${memId}' `;
     console.log(`Loading data ism: '${fromIsmDate}' mem: '${memId}'`);
