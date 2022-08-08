86e1e6a55bec7068f025820fae92c83c8a97759d
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-08-08 22:10:58 +0700

    cancel ticket implementation

--- src/controllers/sale.js
+++ src/controllers/sale.js
@@ -4,6 +4,8 @@ const reverse = async (req, res) => {
     console.log("//::::::::::::::CANCEL BILL::::::::::::::");
     const billId = req.body.billId;
     const cdate = req.body.cdate;
+    const sqlDate=new Date().toISOString().slice(0, 19).replace('T', ' ');
+    console.log("SQL DATE: "+sqlDate);
     console.log("------" + cdate);
     console.log(":::::::::::::::INVESTIGATE IF THE ISM IS CLOSE::::::::::");
     const sql_investigate = `SELECT s.mem_id,i.ism_active FROM sale s LEFT JOIN installment i ON s.ism_id= i.ism_ref WHERE s.sale_bill_id= ${billId}`;
