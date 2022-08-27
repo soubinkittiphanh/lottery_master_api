b87991f44af3d787e4cb2bffb8ac32539a963136
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-08-27 22:37:08 +0700

    query user profile comsale

--- src/controllers/signon.js
+++ src/controllers/signon.js
@@ -7,12 +7,12 @@ const login = async (req, res) => {
     console.log("Login....");
     const { userId, password } = req.body;
     if (!(userId && password)) return res.json({ "status": "01", "desc": "No required data" });
-    const sqlCom = `SELECT mem_id,brc_code,group_code,mem_pass,mem_name,mem_lname,mem_tel FROM member WHERE mem_id ='${userId}'`;
+    const sqlCom = `SELECT mem_id,com_sale,brc_code,group_code,mem_pass,mem_name,mem_lname,mem_tel FROM member WHERE mem_id ='${userId}'`;
     Db.query(sqlCom, (er, re) => {
         if (er) return res.json({ status: "05", desc: er })
         if (re.length==0) return res.json("Error: ໄອດີບໍ່ ຖືກຕ້ອງ")
         const realPassword = re[0]["mem_pass"];
-        const credential = { userId: re[0]["mem_id"], brcCode: re[0]["brc_code"], groupCode: re[0]["group_code"], userName: re[0]["mem_name"], userLname: re[0]["mem_lname"], userTel: re[0]["mem_tel"], }
+        const credential = { userId: re[0]["mem_id"], brcCode: re[0]["brc_code"], groupCode: re[0]["group_code"], userName: re[0]["mem_name"], userLname: re[0]["mem_lname"], userTel: re[0]["mem_tel"],comSate:re[0]["com_sale"] }
         try {
             const isAuth = bcrypt.compare(password, realPassword)
             if (isAuth) {
